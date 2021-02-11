9edbabce70a61e7c55a71f3369d495b099de2f6a
EC2 Default User <ec2-user@ip-172-31-21-108.us-east-2.compute.internal>
2021-02-11 22:27:46 +0000

    test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ActiveRecord::Base
+    def self.with_ratings(ratings_list)
+    # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
+    #  movies with those ratings
+    # if ratings_list is nil, retrieve ALL movies
+    end
 end
