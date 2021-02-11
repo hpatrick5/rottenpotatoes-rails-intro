63d6c8e34f71ae16541f5219583b3d1cb3f8eb79
EC2 Default User <ec2-user@ip-172-31-21-108.us-east-2.compute.internal>
2021-02-11 22:44:10 +0000

    test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,7 @@ class MoviesController < ApplicationController
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
+    @all_ratings = Movie.all_ratings
     # will render app/views/movies/show.<extension> by default
   end
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,8 @@
 class Movie < ActiveRecord::Base
+    def self.all_ratings()
+        return ['G','PG','PG-13','R']
+    end
+    
     def self.with_ratings(ratings_list)
     # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
     #  movies with those ratings
